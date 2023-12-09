f8d1d9d0e1a60bdcef32c9d40ea87186a491a1e3
Undefined <www.undefined@gmail.com>
2023-12-09 19:17:17 +0530

    Added Root Query structure

--- schemas/schema.js
+++ schemas/schema.js
@@ -19,4 +19,20 @@ const UserType = new GraphQLObjectType({
             type: GraphQLInt
         } 
     }
+});
+
+// added new query for fetching user data
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {id: {type: GraphQLString}},
+            // parent value => not ever being use ever
+            // args => which we pass (arguments) (id)
+            resolve(parentValue, args){ // this method actually grab out the data
+
+            }
+        }
+    }
 })
