f52e9055cd8c2e089dab3430f89a6a1cde8960b6
Undefined <www.undefined@gmail.com>
2023-12-09 20:16:42 +0530

    Added nested query, company graphqlobject

--- schemas/schema.js
+++ schemas/schema.js
@@ -8,6 +8,21 @@ const {
     GraphQLSchema
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: {
+            type: GraphQLString
+        },
+        name: {
+            type: GraphQLString
+        },
+        description: {
+            type: GraphQLString
+        }
+    }
+})
+
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
@@ -19,7 +34,10 @@ const UserType = new GraphQLObjectType({
         },
         age: {
             type: GraphQLInt
-        } 
+        },
+        company: {
+            type: CompanyType
+        }
     }
 });
 
